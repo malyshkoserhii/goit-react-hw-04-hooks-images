0bac3ff2509077fd3d25e580065af248be2460d6
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-12-21 22:32:11 +0200

    Modal issue has been fixed

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { usEffect } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
@@ -6,25 +6,25 @@ import s from './Modal.module.css';
 const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ onCloseModal, children }) => {
-  usEffect(() => {
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.code === 'Escape') {
+        onCloseModal();
+      }
+    }
+
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  }, [onCloseModal]);
 
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onCloseModal();
-    }
-  };
-
-  const handleOnBackdropClick = e => {
+  function handleOnBackdropClick(e) {
     if (e.currentTarget === e.target) {
       onCloseModal();
     }
-  };
+  }
 
   return createPortal(
     <div className={s.Overlay} onClick={handleOnBackdropClick}>
